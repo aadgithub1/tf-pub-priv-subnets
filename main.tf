a74ffceaf1880098c2d45a2f82e28e5d2822e45d
aaron <aaronwebb2000@gmail.com>
2026-03-26 12:28:27 -0400

    Successfully deploy pub subnets.

--- main.tf
+++ main.tf
@@ -2,11 +2,37 @@ provider "aws" {
   region = "us-east-2"
 }
 
+// TO-DO
+# make two pub subnets
+# put one pub subnet in each availability zone (us-east-2a and us-east-2b)
+# make two private subnets
+# put one private subnet in each availability zone (us-east-2a and us-east-2b)
+
 resource "aws_vpc" "main" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
 
   tags = {
     Name = var.vpc-name
+  }
+}
+
+resource "aws_subnet" "pub_subnet_a" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.0.0/24"
+  availability_zone = "us-east-2a"
+
+  tags = {
+    Name = "public-subnet-a"
+  }
+}
+
+resource "aws_subnet" "pub_subnet_b" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-2b"
+
+  tags = {
+    Name = "public-subnet-b"
   }
 }
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,19 @@
 output "vpc-id" {
   value = aws_vpc.main.id
 }
+
+output "pub-subnet-a-id" {
+  value = aws_subnet.pub_subnet_a.id
+}
+
+output "pub-subnet-b-id" {
+  value = aws_subnet.pub_subnet_b.id
+}
+
+# output "priv-subnet-a-id" {
+#   value = aws_subnet.priv_subnet_a.id
+# }
+
+# output "priv-subnet-b-id" {
+#   value = aws_subnet.priv_subnet_b.id
+# }
